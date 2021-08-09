8b5edfd2afce1d4262b0bd3247db97a99c8447ba
Kailash Viswanathan <kailashiviswanathan@gmail.com>
2021-08-09 20:56:01 +0530

    Hospital Management Book Appointment added

--- hospital_appointment_booking/main.py
+++ hospital_appointment_booking/main.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 #SQL Connection
 global conn,cursor
 conn = mysql.connector.connect(
-    host='localhost', database='hospital', user='root', password='kailashroot')
+    host='localhost', database='hospital', user='root', password='kailashroot', buffered=True)
 cursor = conn.cursor()
 
-global doc_list
+global doc_list, doc_details
 doc_list = []
+doc_details =[]
 @app.route('/back')
 def maini():
     return render_template('index.html')
@@ -23,22 +24,40 @@ def main():
 @app.route('/result.html', methods = ['POST', 'GET'])
 def result():
     select = request.form.get('option')
-    #data = request.form['input']
 
     if select == 'Book an Appointment':
-        sql = 'select dname from doctors_list;'
+        sql = 'select dname from doctors_list'
         cursor.execute(sql)
         doctors = cursor.fetchall()
         for doctor in doctors:
             doc_list.append(doctor)
         return render_template('book_appointment.html', result = doc_list)
+    else:
+        sql = 'select dname, dskill from doctors_list'
+        cursor.execute(sql)
+        doctors = cursor.fetchall()
+        for doctor in doctors:
+            doc_details.append([doctor[0], doctor[1]])
+        return render_template('search_doctors.html', result = doc_details)
+
     
 @app.route('/booking.html', methods = ['POST', 'GET'])
 def booking():
-    select = request.form.get('option')
+    doc_name = request.form.get('option')
+    doc_name = doc_name[1:-2]
+    book_date = request.form['book_date']
+    sql = "select * from booking_details where dname ={} and booking_date ='{}'".format(doc_name, book_date)
+    cursor.execute(sql)
+    rows = cursor.rowcount
 
-    if select == 'chandru':
-        print("GOT to CHANDRU");
+    if rows < 1:
+        cursor.execute("insert into booking_details(dname, booking_date) values({},'{}')".format(doc_name, book_date))
+        booking_result = "Successfully Booked"
+    else:
+        booking_result = "Doctor has other Appointments try another date"
+    conn.commit()
+    return render_template('booking_result.html', booking = booking_result)
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
